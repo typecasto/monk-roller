2f660eb518c158dceb6e43018051b774f3b01061
Violet <larsona@outlook.com>
2025-04-13 17:54:37 -0500

    level up dude

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use cliclack as cc;
 use ndm::Dice;
 
-const ATTACK: i32 = 25;
+const ATTACK: i32 = 27;
 fn main() -> anyhow::Result<()> {
     let mut total_dmg = 0;
     ctrlc::set_handler(|| {})?;
     cc::intro("dmg-roll")?;
     let attack_type = cc::select("Attack type?")
-        .item("fist", "Powerful Fist", "+25/21/17")
-        .item("laser", "Laser Beam", "+25/21/17")
+        .item("fist", "Powerful Fist", "+27/23/19")
+        .item("laser", "Laser Beam", "+28/24/20")
         .interact()?;
     let buffed = cc::confirm("Inner Upheavel?").interact()?;
     let fob = cc::confirm("Flurry of Blows?")
@@ -20,10 +20,18 @@ fn main() -> anyhow::Result<()> {
         .item(-8, "-8", "")
         .interact()?;
     let d1: i32 = roll("1d20");
-    let mut hits = vec![(d1 + ATTACK + map + if buffed { 1 } else { 0 }, d1)];
+    let mut hits = vec![(
+        d1 + ATTACK + map + if buffed { 1 } else { 0 } + if attack_type == "fist" { 1 } else { 0 },
+        d1,
+    )];
     if fob {
         let d2: i32 = roll("1d20");
-        hits.push((d2 + ATTACK + map - 4 + if buffed { 1 } else { 0 }, d2))
+        hits.push((
+            d2 + ATTACK + map - 4
+                + if buffed { 1 } else { 0 }
+                + if attack_type == "fist" { 1 } else { 0 },
+            d2,
+        ))
     }
     let dmg_die = cc::select("Damage die?")
         .item("1d6", "1d6", "Standard")
@@ -35,7 +43,8 @@ fn main() -> anyhow::Result<()> {
         results.push(
             cc::select(format!("Does a {} hit? (rolled: {})", hit, roll))
                 .item(Some(false), "Hit", "")
-                .item(None, "Miss", "").initial_value(None)
+                .item(None, "Miss", "")
+                .initial_value(None)
                 .item(Some(true), "Crit", "")
                 .interact()?,
         );
@@ -49,8 +58,8 @@ fn main() -> anyhow::Result<()> {
         ];
         match attack_type {
             // .into_iter()
-            "laser" => dmg[1].1.push("3"), // 3 fire
-            "fist" => dmg[0].1.push("9"),  // 6 normal
+            "laser" => dmg[1].1.push("6"), // 3 fire
+            "fist" => dmg[0].1.push("12"), // 6 normal
             _ => {}
         }
         if buffed {
@@ -66,7 +75,7 @@ fn main() -> anyhow::Result<()> {
                         .map(roll)
                         .map(|x| if crit { x * 2 } else { x })
                         .collect::<Vec<_>>() // roll them NOW to collapse the states
-                        .into_iter()
+                        .into_iter(),
                 )
             })
             // .inspect(|x| println!("{:?}", x))
@@ -79,7 +88,8 @@ fn main() -> anyhow::Result<()> {
                 (
                     dice
                         // .inspect(|x| println!("{:?}", x))
-                        .fold(first, |acc, x| acc + " + " + &x.to_string()) + ty,
+                        .fold(first, |acc, x| acc + " + " + &x.to_string())
+                        + ty,
                     sum,
                     ty,
                 )
@@ -106,16 +116,21 @@ fn main() -> anyhow::Result<()> {
         )?;
         if crit {
             cc::log::info("crit! Deal 2d10 persistent fire damage")?;
-            cc::log::info("crit! Target is stupefied 1, and frightened 2 if they were already stupified.")?;
+            cc::log::info(
+                "crit! Target is stupefied 1, and frightened 2 if they were already stupified.",
+            )?;
             if !cc::confirm("crit! Did target succeed a fortitude save vs. your class DC?")
                 .initial_value(true)
                 .interact()?
             {
-                cc::log::info(format!("crit! Target is {} 1 until the end of your next turn.", match attack_type {
-                    "laser" => "dazzled",
-                    "fist" => "slowed",
-                    _ => "huh whuh"
-                }))?;
+                cc::log::info(format!(
+                    "crit! Target is {} 1 until the end of your next turn.",
+                    match attack_type {
+                        "laser" => "dazzled",
+                        "fist" => "slowed",
+                        _ => "huh whuh",
+                    }
+                ))?;
             };
         }
     }
@@ -126,7 +141,9 @@ fn main() -> anyhow::Result<()> {
             .item(3, "Critical Failure", "stunned 3")
             .interact()?;
         if stun != 0 {
-            cc::log::info(format!("Target is stunned {stun} until the end of your next turn."))?;
+            cc::log::info(format!(
+                "Target is stunned {stun} until the end of your next turn."
+            ))?;
         }
     }
     let _ = cc::outro(format!("total dmg: {}", total_dmg));
